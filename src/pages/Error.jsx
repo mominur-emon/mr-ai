da77aca26fc88ebc55898e1b253ea54ccd2b77c3
mominur-emon <mominur.emon19@gmail.com>
2024-03-17 13:35:46 +0600

    react router dom , tailwind css

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+const Error = () => {
+  return (
+    <div>
+      <p className="flex justify-center mt-5 bg-red-500 text-white py-2">
+        Error Pages
+      </p>
+    </div>
+  );
+};
+
+export default Error;
